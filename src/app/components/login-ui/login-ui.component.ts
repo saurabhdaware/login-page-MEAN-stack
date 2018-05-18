899ff982852e55e416508d25e6ef79a95edeaf4f
S4URABH <30949385+S4URABH@users.noreply.github.com>
2018-05-18 16:46:14 +0530

    expressJS code added / HttpModule added to fronend

--- src/app/components/login-ui/login-ui.component.ts
+++ src/app/components/login-ui/login-ui.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient,HttpHeaders, HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-login-ui',
@@ -7,14 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginUiComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
   }
 
   submitLoginForm(loginForm){
-    console.log(loginForm.form.value.email);
-    console.log(loginForm.form.value.pass);
+    // console.log(loginForm.form.value.email);
+    // console.log(loginForm.form.value.pass);
+    this.http.post('http://localhost:3000/',{
+      email:loginForm.form.value.email,
+      pass:loginForm.form.value.pass
+    }).subscribe(data=>console.log(data));
   }
 
 }
